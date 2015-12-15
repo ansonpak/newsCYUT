b07083518f19f9e6023b52f39fd4351bc452e003
anson <anson@anson-HP-Compaq-dx7510-MT>
2015-12-15 13:53:39 +0800

    new

--- newsCYUT/news/migrations/0001_initial.py
+++ newsCYUT/news/migrations/0001_initial.py
@@ -2,24 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='News',
+            name='Latestnews',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('title', models.CharField(max_length=128)),
                 ('content', models.TextField()),
-                ('uploadDate', models.DateTimeField()),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('uploadDate', models.DateField()),
             ],
         ),
     ]
